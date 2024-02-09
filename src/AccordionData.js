3dae5b464a625707040abc93fe0c21b4f7f15b0c
raghav-1998 <raghav.vasdeva@gmail.com>
2024-02-09 20:56:48 +0530

    Creating data for Accrodion

---
+++ src/AccordionData.js
@@ -0,0 +1,23 @@
+const accordionData=[
+    {
+        title:'Section1',
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus purus nec elit volutpat, a dapibus mauris luctus. Aliquam quis purus risus. Integer quis sapien nec velit malesuada ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices eros neque, fringilla tincidunt sapien pretium vitae. Ut pretium arcu tellus, tristique fermentum risus efficitur tristique. Phasellus hendrerit dui at sem convallis sodales. Mauris diam est, dignissim et purus sed, posuere facilisis dui. Nulla facilisi. Donec quis massa euismod, bibendum magna vel, tristique ante. Aliquam aliquet mauris at quam eleifend vulputate.Vivamus facilisis ligula enim, vel aliquet neque suscipit vehicula. Fusce."
+    },
+
+    {
+        title:'Section 2',
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultricies, urna vel lobortis tempor, mauris elit congue sem, quis porttitor velit ligula sit amet metus. Cras suscipit convallis turpis non porttitor. Proin sed massa a ante fermentum vestibulum. Nulla dapibus, sapien ut cursus malesuada, ipsum augue ultricies turpis, ac suscipit metus lorem sed purus. Vivamus mattis pellentesque enim, facilisis consequat nisi sollicitudin id. Nunc in varius mi. Etiam suscipit fringilla lacus, ac facilisis nibh vestibulum id. Curabitur quis ligula nec enim feugiat euismod nec vel dolor. Suspendisse tellus lacus, consectetur at facilisis nec, pulvinar ac odio. Suspendisse a posuere odio."
+    },
+
+    {
+        title:'Section 3',
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus metus non quam efficitur viverra. Sed libero nulla, congue quis enim nec, elementum auctor neque. Aliquam tellus lorem, semper vel sapien nec, porta lacinia libero. Duis ex lorem, dapibus vel posuere ut, mattis aliquet mi. Aliquam quis malesuada purus, non fermentum eros. Integer in eleifend lacus. Mauris at libero eget enim aliquet accumsan. Nunc felis urna, ultricies non luctus vel, dictum vitae tellus. Etiam tempus ante vel lectus venenatis elementum. Aliquam erat volutpat.Donec imperdiet nibh non neque fringilla, non rutrum felis faucibus. Suspendisse gravida elementum nisl, sit amet rhoncus magna elementum non. Sed pellentesque justo commodo tortor porttitor gravida. Etiam urna mi, ultricies non tincidunt et, tempus sed odio. Proin interdum dui non diam posuere tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc porttitor eros quis mauris tincidunt fringilla. Integer condimentum ex at dui cursus."
+    },
+
+    {
+        title:"Section 4",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec feugiat lectus. Cras sit amet felis sollicitudin, scelerisque metus et, bibendum massa. In et commodo leo, eget placerat massa. In quis mattis ipsum. Quisque dictum molestie dui. Duis feugiat sem id mollis dictum. Duis in felis nisl. Ut ut mauris eu massa auctor placerat sed sed est. Quisque tincidunt urna non nisi semper, vel tempor nibh sagittis. Vestibulum hendrerit dui vitae facilisis facilisis. Pellentesque quam leo, finibus eu neque ac, rhoncus pulvinar est. Aliquam posuere a dui id condimentum. Integer sed aliquet turpis. Morbi nulla justo, condimentum id tincidunt at, feugiat eget augue. Quisque ac ante felis. Nunc tempus dui dui, tincidunt tincidunt mauris porttitor nec. Nulla pellentesque sem vel massa congue rutrum. Nulla facilisi. Aliquam rhoncus egestas faucibus. Donec consectetur nunc in ipsum eleifend, commodo placerat velit mollis. Ut et enim sagittis, accumsan velit nec, consequat augue. Phasellus eget sapien lacus. In iaculis ligula eu tellus sollicitudin dapibus. Aliquam dolor erat, lobortis quis aliquam eget, interdum ac risus. In feugiat augue justo, eu consequat quam laoreet et. Donec ornare malesuada luctus. Vivamus est diam, posuere quis semper eu, egestas ut mauris. Sed finibus enim ante, nec aliquet enim dignissim."
+    }
+];
+
+export default accordionData;
